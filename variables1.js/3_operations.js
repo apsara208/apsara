cab0a7ac4015d02aa85ca29b89a450589220155b
apsara208 <apsarachhetri40@gmail.com>
2025-02-15 16:54:55 +0000

    operations

---
+++ variables1.js/3_operations.js
@@ -0,0 +1,10 @@
+let value=3
+let negValue= -value
+console.log(negValue)
+//console.log(2+2) or so
+let str1="hi "
+let str2="apsara"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)// ==12
+console.log(1+2)//3
