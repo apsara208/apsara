cf7cb92a8a9667e0d6e30a3c8d1a3ae2649fd04e
apsara208 <apsarachhetri40@gmail.com>
2025-02-19 17:57:19 +0000

    1 math

--- variables1.js/6_nums_and_math.js
+++ variables1.js/6_nums_and_math.js
@@ -19,7 +19,14 @@ const hundreds =100000
 console.log(hundreds.toLocaleString())//commas in between according to the us standart of counting
 console.log(hundreds.toLocaleString('en-IN'))//commas acc to indian standard
 
-//+++++++++++++++Maths++++++++++++++++++++++
+//+++++++++++++++++Maths++++++++++++++++++++++
 console.log(Math)//math is an object
 console.log(Math.abs(-4))//4,gives absolute value
 console.log(Math.abs(4))//4
+console.log(Math.round(1.2))//roundoff
+console.log(Math.ceil(1.2))//anything above n is n+1,ceil==up
+console.log(Math.floor(1.7))//1,bottom value
+console.log(Math.min(1,8,0,4,7,15))
+console.log(Math.max(1,8,4,7,15))
+console.log(Math.random())//in between 0 and 1
+console.log((Math.random()*10)+1)
