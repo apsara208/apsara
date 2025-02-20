edd2d971a5088e4e4608187abead5a7310c92001
apsara208 <apsarachhetri40@gmail.com>
2025-02-20 13:59:06 +0000

    nums2

--- variables1.js/6_nums_and_math.js
+++ variables1.js/6_nums_and_math.js
@@ -5,7 +5,7 @@ console.log(balance)
 const day={morning:"good",
     noon:"sunny", night:"cold"
 }
-console.log(day)
+/*console.log(day)
 console.log(balance.toString())
 console.log(day.morning.toString())
 console.log(balance.toFixed(2))//rounds off after the decimal
@@ -18,7 +18,7 @@ console.log(otherNumber.toPrecision(2))//rounds off in this number of digits
 const hundreds =100000
 console.log(hundreds.toLocaleString())//commas in between according to the us standart of counting
 console.log(hundreds.toLocaleString('en-IN'))//commas acc to indian standard
-
+*/
 //+++++++++++++++++Maths++++++++++++++++++++++
 console.log(Math)//math is an object
 console.log(Math.abs(-4))//4,gives absolute value
@@ -30,3 +30,7 @@ console.log(Math.min(1,8,0,4,7,15))
 console.log(Math.max(1,8,4,7,15))
 console.log(Math.random())//in between 0 and 1
 console.log((Math.random()*10)+1)
+const min=10
+const max=20
+//Math.random()
+console.log(Math.floor(Math.random() * (max-min+1))+min)
