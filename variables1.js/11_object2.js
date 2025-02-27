4ca56777772d549313b5153d263d8e10a9fc4577
apsara208 <apsarachhetri40@gmail.com>
2025-02-27 17:56:54 +0000

    object functions

--- variables1.js/11_object2.js
+++ variables1.js/11_object2.js
@@ -16,3 +16,35 @@ const bumbleuser={
 }
 console.log(bumbleuser.fullname.usefullname.firstname)
 //? tp check in it whether the value is there or not
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",1:"b"}
+
+//const obj3={obj1,obj2}
+//the objects are stored as different objects inside the new assigned object
+//const obj3=Object.assign({},obj1,obj2)//{}
+//assigns or appends the object according to the keys of the object
+//returns a modified target object
+const  obj3={...obj1,...obj2}
+//spread operator
+console.log(obj3)
+const users =[
+    {
+        id:1,
+        name:"apsara"
+    },
+    {
+
+    },
+    {
+
+    }
+]
+//console.log(users[1].name)
+console.log(tinderuser)
+console.log(Object.keys(tinderuser))
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))
+//each entry or key-value pair is made into an array inside the array
+console.log(tinderuser.hasOwnProperty("isLoggedIn"))
+//to check whether it exists int the object or not 
+//boolean output
