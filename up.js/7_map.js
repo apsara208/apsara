c2bbeafdd24d5d1652a50d257445473a82862e27
apsara208 <apsarachhetri40@gmail.com>
2025-03-23 08:04:04 +0000

    multiple map functions

---
+++ up.js/7_map.js
@@ -0,0 +1,6 @@
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+// const newnums=myNums.map((num)=>num+10)
+// console.log(newnums)
+// l
+const newnums1=myNums.map((num)=>num*10) .map((num)=>num+1)
+console.log(newnums1);
